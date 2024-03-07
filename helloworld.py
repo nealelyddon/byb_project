2751ad02e90b14fd32b1bc726398ce89a58f03d9
Neale John Lydddon <nealelyddon@gmail.com>
2024-03-07 07:41:44 +0000

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,15 @@
-print("Hello world")
+# Input validation function
+def validate_name(name): # Checks if the input contains only alphabetic characters
+    if name.isalpha() or ' ' in name:
+        return True
+    else:
+        return False
 
+# Prompt user for a valid name
+user_input = input("Please enter your name: ")
+while not validate_name(user_input): # Script repeatedly prompts the user until a valid name is entered
+    print("Invalid input. Please enter a valid name.")
+    user_input = input("Please enter your name: ")
 
-user_input= input("Please enter your name:")
+# Greet the user
 print("Hello! " + user_input)
